a6e789686113ef730d4c79ba73d7fcdfc03480f5
Prasanna Patnaik <prasannapatnaik4@gmail.com>
2026-07-20 11:34:17 +0530

    [142] [Time Beats: 100.00%] [Memory Beats: 30.59%] - Leet2Hub

---
+++ 01-Arrays-and-Hashing/142. Linked List Cycle II/142. Linked List Cycle II.java
@@ -0,0 +1,39 @@
+1/**
+2 * Definition for singly-linked list.
+3 * class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode(int x) {
+7 *         val = x;
+8 *         next = null;
+9 *     }
+10 * }
+11 */
+12public class Solution {
+13    public ListNode detectCycle(ListNode head) {
+14        ListNode slow = head;
+15        ListNode fast = head;
+16
+17        while (fast != null && fast.next != null) {
+18
+19            slow = slow.next;
+20
+21            fast = fast.next.next;
+22
+23            if (slow == fast) {
+24
+25                slow = head;
+26
+27                while (slow != fast) {
+28
+29                    slow = slow.next;
+30                    fast = fast.next;
+31                }
+32
+33                return slow;
+34            }
+35        }
+36
+37        return null;    
+38    }
+39}
